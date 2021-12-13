1de418406e99b5767ab79da76be60ec54dc105dd
tejaswinikakade <tejukakade1821@gmail.com>
2021-12-13 23:39:46 +0530

    Added: Validation for first name

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserRegistrationUsingRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the user registration problem");
+            Console.WriteLine("Enter the first name = ");
+            string firstName = Console.ReadLine();
+            UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
+            Console.WriteLine(userRegistrationRegex.validateFirstName(firstName));
+        }
+    }
+}
